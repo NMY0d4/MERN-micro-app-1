fe98ca058a69a995a9a1efe17f1d1cf1d56423b0
Grégory Marini <greg.marini@hotmail.fr>
2023-02-22 15:32:33 +0100

    add CommentCreate et CommentsList component(client)

--- comments/index.js
+++ comments/index.js
@@ -17,11 +17,11 @@ app.get("/posts/:id/comments", (req, res) => {
 
 app.post("/posts/:id/comments", (req, res) => {
     const commentId = randomBytes(4).toString("hex");
-    const { content } = req.body;
+    const { comment } = req.body;
 
     const comments = commentsByPostId[req.params.id] || [];
 
-    comments.push({ commentId, content });
+    comments.push({ commentId, comment });
 
     commentsByPostId[req.params.id] = comments;
 
