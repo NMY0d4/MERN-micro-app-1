be1d812354c9edc29d5e97b0e974406382df51bd
Grégory Marini <greg.marini@hotmail.fr>
2023-02-22 11:29:47 +0100

    introduce client side(reaact/bootstrap) and configure cors

--- comments/index.js
+++ comments/index.js
@@ -1,31 +1,33 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const {randomBytes} = require('crypto');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
 
 const app = express();
-const PORT = 4001;
 app.use(bodyParser.json());
+app.use(cors());
+
+const PORT = 4001;
 
 const commentsByPostId = {};
 
-app.get('/posts/:id/comments', (req,res) => {
-    res.send(commentsByPostId[req.params.id] || [])
-})
+app.get("/posts/:id/comments", (req, res) => {
+    res.send(commentsByPostId[req.params.id] || []);
+});
 
-app.post('/posts/:id/comments', (req,res) => {
-    const commentId = randomBytes(4).toString('hex');
-    const {content} = req.body;
+app.post("/posts/:id/comments", (req, res) => {
+    const commentId = randomBytes(4).toString("hex");
+    const { content } = req.body;
 
     const comments = commentsByPostId[req.params.id] || [];
-    
-    comments.push({commentId, content});
-    
+
+    comments.push({ commentId, content });
+
     commentsByPostId[req.params.id] = comments;
 
     res.status(201).send(comments);
-    
-})
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`);
-})
+});
